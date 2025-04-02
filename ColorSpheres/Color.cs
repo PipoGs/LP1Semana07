67a32ce6281683ee26ec8eeb0830492228fea22d
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-04-02 19:20:17 +0100

    Added Color and Sphere classes

---
+++ ColorSpheres/Color.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColorSpheres
+{
+    public class Color
+    {
+        private byte _red;
+        private byte _blue;
+        private byte _green;
+        private byte _alpha;
+    }
+}
