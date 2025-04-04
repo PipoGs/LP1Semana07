67c822d5351b1875ab15c46c2c466f3d5a575258
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-04-04 17:07:59 +0100

    Added Health,Xp and level properties

--- MyRoguelike/Hero.cs
+++ MyRoguelike/Hero.cs
@@ -7,6 +7,42 @@ namespace MyRoguelike
 {
     public class Hero
     {
+        private float _health;
+        private int _xp;
+        private readonly string _name;
+        private readonly string _level;
 
+        public float Health
+        {
+            get => _health;
+            set
+            {
+                if (value > 0)
+                {
+                    _health = value;
+                }
+                else
+                {
+                    _health = 0;
+                }
+            }
+        }
+
+        public int Xp
+        {
+            get => _xp;
+            set
+            {
+                if (value > 0)
+                {
+                    _xp = value;
+                }
+
+            }
+        }
+
+        public int Level
+        {
+            get => 1 + GetXp() / 1000;
+        }
     }
-}
